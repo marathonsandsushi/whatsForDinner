619685c7328e6628c2973d46d04caaf99633980d
Madison Kendall <lilyzkendall@gmail.com>
2022-03-24 00:07:14 -0600

    responses on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,43 @@
+let foodCravingVal;
+let allergies;
+
 
 function getCravingInput(){
-  let foodCraving = document.getElementById("#cravingInput");
-  console.log("inside getCravingInput");
+  foodCravingVal = document.getElementById("cravingInput").value;
+  // console.log(`inside getCravingInput:  ${foodCravingVal}`);
 }
 
 function getAllergiesInput(){
-  let allergies = document.getElementById("#allergiesInput");
-  console.log("inside getAllergiesInput");
+  allergies = document.getElementById("allergiesInput").value;
+  // console.log(`inside getAllergiesInput:  ${allergies}`);
 }
 
-// based on edamam swagger
-function getEdamamApi (){
 
-  const container = document.getElementById("results - container");
+function getEdamamApi (){
 
   let edamamApiKey = "049b103b5a5a7b73695ba6fb6ad10e2d";
   let edamamIdKey = "0cf3bef7";
-  let foodType='pizza';
+  // let foodType='pizza';
 
-  // https://developer.edamam.com/admin/applications
+  // https://developer.edamam.com/admin/applications -- open "get tailored recipes" for app_id & key
   // Edamam "Recipes" Swagger Page:  https://developer.edamam.com/edamam-docs-recipe-api#/
   // See also: https://codesandbox.io/s/chd5o?file=/src/index.js
 
-    const requestUrl= `https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=${foodType}&app_id=${edamamIdKey}&app_key=${edamamApiKey}&ingr=5-7&mealType=Breakfast&from=0&to=10`;
+    const requestUrl= `https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=${foodCravingVal}&app_id=${edamamIdKey}&app_key=${edamamApiKey}&ingr=5-7&mealType=Breakfast&from=0&to=10`;
     console.log(`requestUrl:  ${requestUrl}`);
 
-// { mode: 'no-cors'}
     fetch(requestUrl ).then((response) => {
       return response.json();
     }).then((json) => {
+      const container = document.getElementById("container");
       json.hits.forEach((data) => {
-        console.log(`${data.recipe.label}:  ${data.recipe.url}`);
-        // container.innerHTML += `
-        //   <p>${data.recipe.label}</p>
-        //   <p>${data.recipe.source}</p>
-        //   <p><a href="${data.recipe.url}">Link to recipe</a></p>
-        //   <img src="${data.recipe.image}" />
-        //   <p>Cooking time: ${data.recipe.totalTime} minutes</p>`;
+        // console.log(`${data.recipe.label}:  ${data.recipe.url}`);
+        container.innerHTML += `
+          <p>${data.recipe.label}</p>
+          <p>${data.recipe.source}</p>
+          <p><a href="${data.recipe.url}">Link to recipe</a></p>
+          <img src="${data.recipe.image}" />
+          <p>Cooking time: ${data.recipe.totalTime} minutes</p>`;
       });
     }).catch((error) => {
       console.error("Error: ", error);
@@ -44,10 +45,6 @@ function getEdamamApi (){
 
   }
 
-// function getEdamamYoutubeExample() {
-//
-// }
 
 
 getEdamamApi();
-// getEdamamYoutubeExample();
