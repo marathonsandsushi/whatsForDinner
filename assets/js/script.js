6c94771432eaeecaa39feb158f4fdf08f5144109
Madison Kendall <lilyzkendall@gmail.com>
2022-03-23 23:24:04 -0600

    got retrieval from edamam recipe api working

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+
+function getCravingInput(){
+  let foodCraving = document.getElementById("#cravingInput");
+  console.log("inside getCravingInput");
+}
+
+function getAllergiesInput(){
+  let allergies = document.getElementById("#allergiesInput");
+  console.log("inside getAllergiesInput");
+}
+
+// based on edamam swagger
+function getEdamamApi (){
+
+  const container = document.getElementById("results - container");
+
+  let edamamApiKey = "049b103b5a5a7b73695ba6fb6ad10e2d";
+  let edamamIdKey = "0cf3bef7";
+  let foodType='pizza';
+
+  // https://developer.edamam.com/admin/applications
+  // Edamam "Recipes" Swagger Page:  https://developer.edamam.com/edamam-docs-recipe-api#/
+  // See also: https://codesandbox.io/s/chd5o?file=/src/index.js
+
+    const requestUrl= `https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=${foodType}&app_id=${edamamIdKey}&app_key=${edamamApiKey}&ingr=5-7&mealType=Breakfast&from=0&to=10`;
+    console.log(`requestUrl:  ${requestUrl}`);
+
+// { mode: 'no-cors'}
+    fetch(requestUrl ).then((response) => {
+      return response.json();
+    }).then((json) => {
+      json.hits.forEach((data) => {
+        console.log(`${data.recipe.label}:  ${data.recipe.url}`);
+        // container.innerHTML += `
+        //   <p>${data.recipe.label}</p>
+        //   <p>${data.recipe.source}</p>
+        //   <p><a href="${data.recipe.url}">Link to recipe</a></p>
+        //   <img src="${data.recipe.image}" />
+        //   <p>Cooking time: ${data.recipe.totalTime} minutes</p>`;
+      });
+    }).catch((error) => {
+      console.error("Error: ", error);
+    });
+
+  }
+
+// function getEdamamYoutubeExample() {
+//
+// }
+
+
+getEdamamApi();
+// getEdamamYoutubeExample();
